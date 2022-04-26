f63935b93319e6f5b1baeca11cf5291ba41d9c35
EnjoyCpp <63050200+EnjoyCpp@users.noreply.github.com>
2022-04-27 01:21:18 +0300

    Creating setud-ds.js file for creating database

---
+++ database/setup-db.js
@@ -0,0 +1,25 @@
+var sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('chess.db', (err) => {
+    if (err) {
+        console.error("Error opening database " + err.message);
+    } else {
+
+        db.run('CREATE TABLE Chess_openings( \
+            id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\
+            author NVARCHAR(20)  NOT NULL,\
+            title NVARCHAR(20),\
+            year INTEGER\
+        )', (err) => {
+            if (err) {
+                console.log("Table already exists.");
+            }
+            let insert = 'INSERT INTO Chess_openings (author, title, year) VALUES (?,?,?)';
+            db.run(insert, ["Greco","Italian Game",1620]);
+            db.run(insert, ["James Mason","London System",1922]);
+            db.run(insert, ["Horatio Caro and Marcus Kann","Caro–Kann Defence",1886]);
+            db.run(insert, ["Giulio Cesare Polerio","Sicilian Defence",1594]);
+            db.run(insert, ["Ruy López de Segura","Ruy López",1490]);
+        });
+    }
+});
--- index.js
+++ index.js
@@ -17,30 +17,7 @@ app.listen(HTTP_PORT, () => {
     console.log("Server is listening on port " + HTTP_PORT);
 });
 
-const db = new sqlite3.Database('database/chess.db', (err) => {
-    if (err) {
-        console.error("Erro opening database " + err.message);
-    } else {
-
-        db.run('CREATE TABLE Chess_openings( \
-            id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\
-            author NVARCHAR(20)  NOT NULL,\
-            title NVARCHAR(20),\
-            year INTEGER\
-        )', (err) => {
-            if (err) {
-                console.log("Table already exists.");
-            }
-            let insert = 'INSERT INTO Chess_openings (author, title, year) VALUES (?,?,?)';
-            db.run(insert, ["Greco","Italian Game",1620]);
-            db.run(insert, ["James Mason","London System",1922]);
-            db.run(insert, ["Horatio Caro and Marcus Kann","Caro–Kann Defence",1886]);
-            db.run(insert, ["Giulio Cesare Polerio","Sicilian Defence",1594]);
-            db.run(insert, ["Ruy López de Segura","Ruy López",1490]);
-        });
-    }
-});
-
+const db = new sqlite3.Database('database/chess.db')
 
 app.get("/Chess_openings/:id", (req, res, next) => {
     var params = [req.params.id]
@@ -79,10 +56,25 @@ app.post("/Chess_openings/", (req, res, next) => {
 });
 
 
-app.patch("/Chess_openings/", (req, res, next) => {
+app.put("/Chess_openings/", (req, res, next) => {
     var reqBody = req.body;
     db.run(`UPDATE Chess_openings set author = ?, title = ?, year = ? WHERE id = ?`,
         [reqBody.author, reqBody.title, reqBody.year, reqBody.id],
+        function (err, result) {
+            if (err) {
+                res.status(400).json({ "error": res.message })
+                return;
+            }
+            res.status(200).json({ updatedID: this.changes });
+        });
+});
+
+
+app.patch("/Chess_openings/:id/$author/:author", (req, res, next) => {
+    var reqBody = req.body;
+    console.log(reqBody)
+    db.run(`UPDATE Chess_openings set author = ? WHERE id = ?`,
+        [reqBody.author, reqBody.id],
         function (err, result) {
             if (err) {
                 res.status(400).json({ "error": res.message })
